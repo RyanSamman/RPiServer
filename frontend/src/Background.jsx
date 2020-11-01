2edd2f4c78f115081767d11bc6981204878d6c3e
RyanSamman <RyanSamman@outlook.com>
2020-11-01 22:23:08 +0300

    Cleanup code

--- frontend/src/Background.jsx
+++ frontend/src/Background.jsx
@@ -1,15 +1,16 @@
-import './App.css';
+import './App.css'
 import React from 'react'
-import logo from './logo.svg';
-import moon from './Moon.gif';
+import logo from './logo.svg'
+import moon from './Moon.gif'
 
 export default function Background({ state, toggle }) {
 	return (
 		<header className={`App-header ${state === 1 ? 'moonbg' : ''}`} onClick={toggle}>
-			{state === 1
-			 ? <img src={moon} className="moon" alt="logo" />
-			 : <img src={logo} className="App-logo" alt="moon" />
-			 }
+			{
+				state === 1
+					? <img src={moon} className='moon' alt='logo' />
+					: <img src={logo} className='App-logo' alt='moon' />
+			}
 		</header>
-	);
-};
+	)
+}

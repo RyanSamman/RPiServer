ae004d98150496d508c5870c31377858a0498556
RyanSamman <RyanSamman@outlook.com>
2020-11-01 18:43:24 +0300

    text

--- frontend/src/Background.jsx
+++ frontend/src/Background.jsx
@@ -7,6 +7,7 @@ import moon from './Moon.gif';
 export default function Background({ state, toggle }) {
 	return (
 		<header className={`App-header ${state === 1 ? 'moonbg' : ''}`} onClick={toggle}>
+			<h1>CPIT-221</h1>
 			{state === 1
 			 ? <img src={moon} className="moon" alt="logo" />
 			 : <img src={logo} className="App-logo" alt="moon" />

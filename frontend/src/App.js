8eb6e4e0e24113f80beec87fab1cdd83d53f194a
RyanSamman <RyanSamman@outlook.com>
2020-11-01 20:40:21 +0300

    Refactor & Cleanup code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-import Background from './Background';
-import socketIOClient from "socket.io-client";
+import Background from './Background'
+import socketIOClient from "socket.io-client"
 
 // Don't hard-code the URL
-const ENDPOINT = "https://ryans-socket.herokuapp.com/";
+console.log(process.env.REACT_APP_SOCKET_URL)
+const ENDPOINT = process.env.REACT_APP_SOCKET_URL || "https://ryans-socket.herokuapp.com/"
 
-const socket = socketIOClient(ENDPOINT);
+const socket = socketIOClient(ENDPOINT)
 
-const toggle = () => {
-  socket.emit('toggle', true)
-}
+const toggle = () => socket.emit('toggle', true)
 
 function App() {
-  const [state, setState] = useState(-1);
+  const [state, setState] = useState(-1)
 
   useEffect(() => {
-    socket.on('state', (newState) => setState(newState));
-    return () => socket.disconnect();
-  }, []);
+    socket.on('state', (newState) => setState(newState))
+    return () => socket.disconnect()
+  }, [])
 
-  return (
-    <Background state={state} toggle={toggle} />
-  );
+  return <Background state={state} toggle={toggle} />
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,51 +1,62 @@
-const { Gpio } = require('onoff');
-const LED = Gpio(4, 'out');
+// Imports
+const http = require('http')
+const { Gpio } = require('onoff')
+const express = require('express')
+const socketIo = require('socket.io')
 
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-
-const port = process.env.PORT || 4001;
-
-const app = express();
-app.use(express.static('build'));
-
-const server = http.createServer(app);
-
-const io = socketIo(server);
-
-let interval;
-let state = -1;
-
-io.on("connection", (socket) => {
-  console.log("New client connected");
-  socket.emit('state', state);
-  if (interval) {
-    clearInterval(interval);
-  }
-  
-  interval = setInterval(() => getApiAndEmit(socket), 10000);
-
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-    clearInterval(interval);
-  });
-
-  socket.on("toggle", () => {
-    console.log('toggled');
-    getApiAndEmit(socket)
-  })
-});
-
-const getApiAndEmit = (socket) => {
-  state *= -1;
-  io.emit("state", state);
-  LED.writeSync([0, 0, 1][state + 1]); // Enumerate -1, 1 to 0, 1
-};
-
-server.listen(port, () => console.log(`Listening on port ${port}`));
+// Create LED Object to control GPIO
+const LED = Gpio(4, 'out')
 
 process.on('SIGINT', () => {
-  LED.writeSync(0);
-  LED.unexport();
+  LED.writeSync(0)
+  LED.unexport()
 })
+
+// Create express server
+const app = express()
+
+// Route requests to frontend files
+app.use(express.static('build'))
+
+// Create socket server
+const server = http.createServer(app)
+const io = socketIo(server)
+
+// variable to store Interval - Runs callback every 'INTERVAL_TIME' miliseconds
+let interval
+const INTERVAL_TIME = 10000
+
+// Current LED / Website state, -1 by default
+let state = -1
+
+io.on('connection', (socket) => {
+  console.log('New client connected')
+  socket.emit('state', state)
+
+  if (interval) clearInterval(interval)
+  interval = setInterval(() => getApiAndEmit(socket), INTERVAL_TIME)
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected')
+    clearInterval(interval)
+  })
+
+  socket.on('toggle', () => {
+    console.log('toggled')
+    getApiAndEmit(socket)
+  })
+})
+
+const getApiAndEmit = () => {
+   // Easiest way to flip state, * -1
+  state *= -1
+  io.emit('state', state)
+
+  // Map state for LED API
+  // -1 -> 0
+  //  1 -> 1
+  LED.writeSync([0, 0, 1][state + 1]) 
+}
+
+const port = process.env.PORT || 4001
+server.listen(port, () => console.log(`Listening on port ${port}`))
